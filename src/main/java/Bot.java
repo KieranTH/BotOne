5c1eff2629d4f5ef98af31c14b399864e8287614
KieranTH <Kieran.hughes2@live.co.uk>
2020-01-20 20:14:33 +0000

    Music Added

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,7 @@
 import commands.Calculate;
+import commands.IoanSpam;
+import commands.Music.Sound;
+import commands.joke;
 import commands.nicknamer;
 import events.HelloEvent;
 import net.dv8tion.jda.api.JDA;
@@ -9,11 +12,14 @@ public class Bot {
 
     public static void main(String[] args) throws Exception
     {
-        JDA jda = new JDABuilder("NjExODg2Njc2ODcyMjY1NzQ5.XWU3Ig.Rv8SRrGHN-WNtq5qamQsULVil0I").build();
+        JDA jda = new JDABuilder("NjExODg2Njc2ODcyMjY1NzQ5.XiNrRw.F-B8zmXvFywG2y-f-2uhA9A5iLc").build();
 
         jda.addEventListener(new HelloEvent());
         //jda.addEventListener(new nicknameChanger());
         jda.addEventListener(new Calculate());
         jda.addEventListener(new nicknamer());
+        jda.addEventListener(new joke());
+        jda.addEventListener(new Sound());
+        jda.addEventListener(new IoanSpam());
     }
 }
